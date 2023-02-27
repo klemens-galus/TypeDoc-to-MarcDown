113c87b1b2ce41cc54b687d56eaa888f38aedb9b
Klemens Galus <galus.klemens@gmail.com>
2023-02-27 21:10:06 +0100

    Splite files

--- py.py
+++ py.py
@@ -2,79 +2,12 @@ from bs4 import BeautifulSoup
 import urllib.request
 import os
 import pathlib
-class Module:
-    def __init__(self, link, name):
-        self.link = link
-        self.name = name
-    def __repr__(self):
-        return f"Module(link='{self.link}', name='{self.name}')"
 
-def checkClasses(mod):
-    webpage = open("./docs/"+mod.link)
-    soup = BeautifulSoup(webpage, 'html.parser')
-    classes = []
-
-    for anchor in soup.find_all("a", {"class": "tsd-index-link tsd-kind-class tsd-parent-kind-module"}, href=True):
-        link = str(anchor.get('href'))
-        name = str(anchor.find('span'))
-        name = name.replace("<span>", "").replace("<wbr/>", "").replace("</span>", "")
-        classes.append(Module(link,name))
-    for clas in classes:
-        reallink = clas.link.rsplit('/', 1)[-1].replace(".html", ".md")
-        sf = open('/usr/src/app/mdDock/classes/'+reallink,'w')
-        print(clas.link)
-        print(clas.name)
-
-def checkVars(mod):
-    webpage = open("./docs/"+mod.link)
-    soup = BeautifulSoup(webpage, 'html.parser')
-    classes = []
-
-    for anchor in soup.find_all("a", {"class": "tsd-index-link tsd-kind-variable tsd-parent-kind-module"}, href=True):
-        link = str(anchor.get('href'))
-        name = str(anchor.find('span'))
-        name = name.replace("<span>", "").replace("<wbr/>", "").replace("</span>", "")
-        classes.append(Module(link,name))
-    for clas in classes:
-        reallink = clas.link.rsplit('/', 1)[-1].replace(".html", ".md")
-        sf = open('/usr/src/app/mdDock/variables/'+reallink,'w')
-        print(clas.link)
-        print(clas.name)
-
-def checkFunc(mod):
-    webpage = open("./docs/"+mod.link)
-    soup = BeautifulSoup(webpage, 'html.parser')
-    classes = []
-
-    for anchor in soup.find_all("a", {"class": "tsd-index-link tsd-kind-function tsd-parent-kind-module"}, href=True):
-        link = str(anchor.get('href'))
-        name = str(anchor.find('span'))
-        name = name.replace("<span>", "").replace("<wbr/>", "").replace("</span>", "")
-        classes.append(Module(link,name))
-    for clas in classes:
-        reallink = clas.link.rsplit('/', 1)[-1].replace(".html", ".md")
-        sf = open('/usr/src/app/mdDock/functions/'+reallink,'w')
-        print(clas.link)
-        print(clas.name)
-
-def checkTypes(mod):
-    webpage = open("./docs/"+mod.link)
-    soup = BeautifulSoup(webpage, 'html.parser')
-    classes = []
-
-    for anchor in soup.find_all("a", {"class": "tsd-index-link tsd-kind-type-alias tsd-parent-kind-module"}, href=True):
-        link = str(anchor.get('href'))
-        name = str(anchor.find('span'))
-        name = name.replace("<span>", "").replace("<wbr/>", "").replace("</span>", "")
-        classes.append(Module(link,name))
-    for clas in classes:
-        reallink = clas.link.rsplit('/', 1)[-1].replace(".html", ".md")
-        sf = open('/usr/src/app/mdDock/types/'+reallink,'w')
-        print(clas.link)
-        print(clas.name)
-
-
-
+from src.classModule import Module 
+from src.checkFunctions import checkFunc
+from src.checkVars import checkVars
+from src.checkClasses import checkClasses
+from src.checkTypes import checkTypes 
 
 
 webpage = open("./docs/modules.html")
@@ -88,9 +21,6 @@ os.mkdir("/usr/src/app/mdDock/modules")
 os.mkdir("/usr/src/app/mdDock/types")
 os.mkdir("/usr/src/app/mdDock/variables")
 os.mkdir("/usr/src/app/mdDock/functions")
-
-
-
 
 for anchor in soup.find_all("a", {"class": "tsd-index-link tsd-kind-module"}, href=True):
     link = str(anchor.get('href'))
